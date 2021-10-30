e6e3de713a791fd73bebc4c437a145fff304f4ce
milu <milu@atguigu.com>
2021-10-30 19:10:44 +0800

    hot-fix commit1

--- src/main/java/com/milu/git/GitTest.java
+++ src/main/java/com/milu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello,git");
         System.out.println("heih");
         System.out.println("nide");
+        System.out.println("hello ,4");
     }
 }
